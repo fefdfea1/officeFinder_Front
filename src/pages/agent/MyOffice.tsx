dc27f97b7e35fc26b70723e7b2758dc828f56ea9
Jieun Kang <ochoby@gmail.com>
2023-08-16 00:44:22 +0900

    MyOffice, OfficeName 제작 중, BackgroundCover 추가
    backgroundCover 의논 필요

--- src/pages/agent/MyOffice.tsx
+++ src/pages/agent/MyOffice.tsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import { Officename } from '../../components/booking/Officename';
+import { BackgroundCover } from '../../components/common/BackgroundCover';
+import { Title } from '../../components/common/Title';
 
-export default function MyOffice() {
-  return <div>MyOffice</div>;
+export const MyOffice = () => {
+  return (
+    <>
+      <BackgroundCover>
+        <Title>나의 지점보기</Title>
+        <div className="flex gap-4 p-4">
+          <figure className="flex flex-col w-80 gap-1">
+            <img className="rounded-xl" src="https://picsum.photos/350" alt="sample" />
+            <Officename officename="오피스명" address="주소" />
+          </figure>
+        </div>
+      </BackgroundCover>
+    </>
+  );
 }
