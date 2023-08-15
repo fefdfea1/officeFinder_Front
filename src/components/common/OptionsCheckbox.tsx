797baa5b980db65c1e0fabb44680a9772b2c286c
Jieun Kang <ochoby@gmail.com>
2023-08-15 15:43:47 +0900

    OptionCheckbox.tsx 추가

--- src/components/common/OptionsCheckbox.tsx
+++ src/components/common/OptionsCheckbox.tsx
@@ -1,5 +1,153 @@
-import React from 'react';
+export const OptionsCheckbox = () => {
 
-export default function OptionsCheckbox() {
-  return <div>OptionsCheckbox</div>;
+  return (
+    <div className="options border rounded-xl p-4">
+      <div className="grid grid-cols-4">
+
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">에어컨</span>
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">카페</span>
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">복사기</span>
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">택배서비스</span>
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">도어락</span>
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">콘센트</span>
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">팩스</span>
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">24시 운영</span>
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">연중 무휴</span>
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">난방기</span>
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">주차</span>
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">공용 라운지</span>
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">공용 주방</span>
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">정수기</span>
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">루프탑</span>
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">다과, 음료</span>
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">개인락커</span>
+          </label>
+        </div>
+
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">프로젝터</span>
+          </label>
+        </div>
+
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">화이트보드</span>
+          </label>
+        </div>
+
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">인터넷</span>
+          </label>
+        </div>
+
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">샤워시설</span>
+          </label>
+        </div>
+
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" className="checkbox checkbox-xs checkbox-primary" />
+            <span className="label-text">창고</span>
+          </label>
+        </div>
+
+
+
+
+
+      </div>
+
+    </div>
+  )
 }
