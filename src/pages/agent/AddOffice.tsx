14eeb18224aa16321172c45caf5b576806fd8669
Jieun Kang <ochoby@gmail.com>
2023-08-17 00:25:33 +0900

    Update AddOffice.tsx

--- src/pages/agent/AddOffice.tsx
+++ src/pages/agent/AddOffice.tsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { BackgroundCover } from "../../components/common/BackgroundCover";
 import { Title } from "../../components/common/Title";
 import { Button } from "../../components/common/Button";
+import { Input } from "../../components/common/Input";
+import { OptionsCheckbox } from "../../components/common/OptionsCheckbox";
 
 export const AddOffice = () => {
   return (
@@ -16,6 +18,11 @@ export const AddOffice = () => {
       </div>
       <BackgroundCover>
         <Title>새 지점 등록하기</Title>
+        <div className="flex flex-col items-center py-8">
+          <Input inputTitle="공간의 이름을 입력해주세요." placeholder="" warning="" />
+          <Input inputTitle="오피스 주소를 입력해주세요." placeholder="도로명으로 입력해주세요." warning="" />
+          <OptionsCheckbox />
+        </div>
       </BackgroundCover>
     </>
   );
