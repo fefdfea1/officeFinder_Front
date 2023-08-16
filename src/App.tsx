255705f17f1d9256fe22a14c91f0e1f6787682dd
fefdfea <cankyu6@gmail.com>
2023-08-16 15:55:59 +0900

    complict 해결을 위한 commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './css/App.css';
 import Router from './router/Router';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import Nav from './components/common/Nav';
-import Footer from './components/common/Footer';
+import { Footer } from './components/common/Footer';
 
 function App() {
   const queryClient = new QueryClient();
