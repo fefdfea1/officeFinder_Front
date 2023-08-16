525a057c961bd85b63284562775fc9d1ed7c387c
LAPTOP-I2IVEE82\ymy06 <ymy0613@gmail.com>
2023-08-16 16:00:22 +0900

    input button 추가, nav, footer 수정

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -12,8 +12,8 @@ export const Input = (props: InputProps) => {
   <div className='px-4'>
     <p className='text-sm pl-2 mt-0'>{props.inputTitle}</p>
     <input type="text" placeholder={props.placeholder} className="relative input input-bordered input-primary w-full max-w-xs m-2" />
-  </div>
     {/* <p className='text-sm pl-2 text-error'>{props.warning}</p> */}
+  </div>
   </>
   );
 }
