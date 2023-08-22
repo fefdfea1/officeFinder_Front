e1f2b1d0da34664f8205bbb184a15522007026f1
fefdfea <cankyu6@gmail.com>
2023-08-22 20:39:56 +0900

    하트 디자인 및 에니메이션 제작 완료

--- src/components/common/HeartActive.ts
+++ src/components/common/HeartActive.ts
@@ -1,9 +1,9 @@
 export const heartActive = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
   const target = e.target as HTMLElement;
-  console.log(target);
   if (!target.classList.contains('active')) {
     target.classList.add('active');
   } else {
     target.classList.remove('active');
+    target.classList.add('deActive');
   }
 };
