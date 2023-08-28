4219c54f9c874c235527efe1c6b6a466c40c8c2a
kjewt <79249655+kjewt@users.noreply.github.com>
2023-08-28 16:05:00 +0900

    Update AllReviews.tsx

--- src/pages/agent/AllReviews.tsx
+++ src/pages/agent/AllReviews.tsx
@@ -22,7 +22,7 @@ export const AllReviews = () => {
         <div className="absolute top-[-6px] lg:top-10 right-10 flex z-10">
           <div className="flex">
             <div className="group py-4 ">
-              <MyOfficeListDropDown width="md:w-52 w-32" onOfficeChange={handleOfficeChange} />
+              <MyOfficeListDropDown onOfficeChange={handleOfficeChange} />
               <hr className="border-primary group-hover:border-transparent" />
             </div>
             <Link to="/SalesAnalysis">
