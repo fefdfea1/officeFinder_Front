2320ff0c57f4f79e08e3bb18b4015ebc3598deea
LAPTOP-I2IVEE82\ymy06 <ymy0613@gmail.com>
2023-08-29 15:25:27 +0900

    import삭제

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
 import type { ButtonHTMLAttributes } from 'react';
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  text: string,
-  style: any,
-  clickHandler?: () => void,
+  text: string;
+  style: any;
+  clickHandler?: () => void;
 }
 
-export const Button = ({text, style, clickHandler, ...rest} : ButtonProps) => {
+export const Button = ({ text, style, clickHandler, ...rest }: ButtonProps) => {
   return (
-  <>
-  <div className='p-4'>
-    <button onClick={clickHandler} className={style} {...rest}>{text}</button>
-    {/*className="btn btn-outline btn-primary w-72 m-2 text-base" 기본 스타일 입니다. */}
-  </div>
-  </>
-  )
-
-}
+    <>
+      <div className="p-4">
+        <button onClick={clickHandler} className={style} {...rest}>
+          {text}
+        </button>
+        {/*className="btn btn-outline btn-primary w-72 m-2 text-base" 기본 스타일 입니다. */}
+      </div>
+    </>
+  );
+};
