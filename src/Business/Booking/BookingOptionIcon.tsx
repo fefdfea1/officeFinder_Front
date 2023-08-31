4e8016665fb90a74b69b93274d98ce8303911f6e
fefdfea <cankyu6@gmail.com>
2023-08-31 22:13:11 +0900

    BookingOptionIcon 제작 완료

--- src/Business/Booking/BookingOptionIcon.tsx
+++ src/Business/Booking/BookingOptionIcon.tsx
@@ -11,7 +11,7 @@ import { IoIosCafe } from "react-icons/io";
 //팩스
 import { FaFax } from "react-icons/fa";
 //부엌
-import { FaKitchenSet } from "react-icons/Fa6";
+import { FaKitchenSet } from "react-icons/fa6";
 //인터넷
 import { MdOutlineSignalWifiStatusbarConnectedNoInternet4 } from "react-icons/md";
 //복사기
@@ -39,14 +39,84 @@ export const changeOptionName = (data: OptionType) => {
   data.forEach(element => {
     const createObject: ObjectType = {};
     switch (element) {
-      case "haveHeater":
-        createObject.name = "난방기";
-        createObject.Icon = <GiFireplace />;
+      case "haveAirCondition":
+        createObject.name = "에어컨";
+        createObject.Icon = <TbAirConditioningDisabled />;
+        changeArr.push(createObject);
+        break;
+      case "haveCafe":
+        createObject.name = "카페";
+        createObject.Icon = <IoIosCafe />;
+        changeArr.push(createObject);
+        break;
+      case "havePrinter":
+        createObject.name = "복사기";
+        createObject.Icon = <AiFillCopy />;
+        changeArr.push(createObject);
+        break;
+      case "packageSendServiceAvailable":
+        createObject.name = "택배배송";
+        createObject.Icon = <BsFillBoxSeamFill />;
+        changeArr.push(createObject);
+        break;
+      case "haveDoorLock":
+        createObject.name = "도어락";
+        createObject.Icon = <RiDoorLockBoxFill />;
+        changeArr.push(createObject);
+        break;
+      case "faxServiceAvailable":
+        createObject.name = "팩스";
+        createObject.Icon = <FaFax />;
+        changeArr.push(createObject);
+        break;
+      case "havePublicKitchen":
+        createObject.name = "공용주방";
+        createObject.Icon = <FaKitchenSet />;
+        changeArr.push(createObject);
+        break;
+      case "havePublicLounge":
+        createObject.name = "공용라운지";
+        createObject.Icon = <GrLounge />;
+        changeArr.push(createObject);
+        break;
+      case "havePrivateLocker":
+        createObject.name = "개인 사물함";
+        createObject.Icon = <PiLockersFill />;
+        changeArr.push(createObject);
+        break;
+      case "haveTvProjector":
+        createObject.name = "프로젝터";
+        createObject.Icon = <BsFillProjectorFill />;
         changeArr.push(createObject);
         break;
       case "haveWhiteBoard":
         createObject.name = "화이트보드";
         createObject.Icon = <FaChalkboardTeacher />;
+        changeArr.push(createObject);
+        break;
+      case "haveWifi":
+        createObject.name = "인터넷";
+        createObject.Icon = <MdOutlineSignalWifiStatusbarConnectedNoInternet4 />;
+        changeArr.push(createObject);
+        break;
+      case "haveShowerBooth":
+        createObject.name = "샤워실";
+        createObject.Icon = <FaShower />;
+        changeArr.push(createObject);
+        break;
+      case "haveStorage":
+        createObject.name = "창고";
+        createObject.Icon = <TbBuildingWarehouse />;
+        changeArr.push(createObject);
+        break;
+      case "haveHeater":
+        createObject.name = "난방기";
+        createObject.Icon = <GiFireplace />;
+        changeArr.push(createObject);
+        break;
+      case "haveParking":
+        createObject.name = "난방기";
+        createObject.Icon = <FaParking />;
         changeArr.push(createObject);
         break;
     }
