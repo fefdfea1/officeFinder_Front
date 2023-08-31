a612e104389b82b3521726099f789185dd406f6a
fefdfea <cankyu6@gmail.com>
2023-08-31 21:03:34 +0900

    예약페이지 슬라이더 제작 및 로드뷰 툴팁제작 완료하였습니다다

---
+++ src/pages/BookingSlider.tsx
@@ -0,0 +1,52 @@
+import { ReactNode } from "react";
+import Slider from "react-slick";
+import styled from "@emotion/styled";
+
+type BookingSliderType = {
+  children: ReactNode;
+  setting: settingType;
+};
+
+type settingType = {
+  [key: string]: boolean | number;
+};
+
+export const SlickSlider = ({ children, setting }: BookingSliderType) => {
+  return <SliderContainer {...setting}>{children}</SliderContainer>;
+};
+
+const SliderContainer = styled(Slider)`
+  & .slick-arrow {
+    z-index: 1;
+  }
+
+  @media (min-width: 360px) {
+    & .slick-prev {
+      left: 1%;
+    }
+
+    & .slick-next {
+      right: 1%;
+    }
+
+    @media (min-width: 768px) {
+      & .slick-prev {
+        left: 17%;
+      }
+      & .slick-next {
+        right: 17%;
+      }
+    }
+
+  @media (min-width: 1280px) {
+    & .slick-prev {
+      left: 1%;
+    }
+
+    & .slick-next {
+      right: 1%;
+    }
+  }
+
+
+`;
