4408bc18585ce1481841bdc86224206b8494a789
fefdfea1 <46808357+fefdfea1@users.noreply.github.com>
2023-09-05 18:14:19 +0900

    Update AllOfficeList.tsx

--- src/components/common/AllOfficeList.tsx
+++ src/components/common/AllOfficeList.tsx
@@ -31,75 +31,9 @@ export const AllOfficeList = (): JSX.Element => {
                 <p className="font-medium text-sm">{v?.address}</p>
                 <p className="font-medium text-sm price">{v?.price}</p>
               </div>
-
-            </div>
-            <p className="font-medium text-sm">서울시 강남구 테헤란로70번길 14-10</p>
-            <p className="font-medium text-sm">200m</p>
-            <p className="font-medium text-sm price">월 500,000</p>
-          </div>
-        </figure>
-
-        <figure className="mx-auto">
-          <img className="rounded-xl" src="https://picsum.photos/320" alt="Shoes" />
-          <div className="text-left">
-            <div className="flex justify-between flex-row">
-              <p className="font-bold text-base tracking-tight">선릉 더 공간A</p>
-              <div className="flex gap-1 items-center">
-                <AiFillStar className="flex" />
-                <p className="font-bold text-sm tracking-tight">4.91(21)</p>
-              </div>
-            </div>
-            <p className="font-medium text-sm">서울시 강남구 테헤란로70번길 14-10</p>
-            <p className="font-medium text-sm">200m</p>
-            <p className="font-medium text-sm price">월 500,000</p>
-          </div>
-        </figure>
-        <figure className="mx-auto">
-          <img className="rounded-xl" src="https://picsum.photos/320" alt="Shoes" />
-          <div className="text-left">
-            <div className="flex justify-between flex-row">
-              <p className="font-bold text-base tracking-tight">선릉 더 공간A</p>
-              <div className="flex gap-1 items-center">
-                <AiFillStar className="flex" />
-                <p className="font-bold text-sm tracking-tight">4.91(21)</p>
-              </div>
-            </div>
-            <p className="font-medium text-sm">서울시 강남구 테헤란로70번길 14-10</p>
-            <p className="font-medium text-sm">200m</p>
-            <p className="font-medium text-sm price">월 500,000</p>
-          </div>
-        </figure>
-        <figure className="mx-auto">
-          <img className="rounded-xl" src="https://picsum.photos/320" alt="Shoes" />
-          <div className="text-left">
-            <div className="flex justify-between flex-row">
-              <p className="font-bold text-base tracking-tight">선릉 더 공간A</p>
-              <div className="flex gap-1 items-center">
-                <AiFillStar className="flex" />
-                <p className="font-bold text-sm tracking-tight">4.91(21)</p>
-              </div>
-            </div>
-            <p className="font-medium text-sm">서울시 강남구 테헤란로70번길 14-10</p>
-            <p className="font-medium text-sm">200m</p>
-            <p className="font-medium text-sm price">월 500,000</p>
-          </div>
-        </figure>
-        <figure className="mx-auto">
-          <img className="rounded-xl" src="https://picsum.photos/320" alt="Shoes" />
-          <div className="text-left">
-            <div className="flex justify-between flex-row">
-              <p className="font-bold text-base tracking-tight">선릉 더 공간A</p>
-              <div className="flex gap-1 items-center">
-                <AiFillStar className="flex" />
-                <p className="font-bold text-sm tracking-tight">4.91(21)</p>
-              </div>
-            </div>
-            <p className="font-medium text-sm">서울시 강남구 테헤란로70번길 14-10</p>
-            <p className="font-medium text-sm">200m</p>
-            <p className="font-medium text-sm price">월 500,000</p>
-          </div>
-        </figure>
-
+            </figure>
+          );
+        })}
       </div>
       <BackgroundCover width="w-1/3" margin="mx-auto" padding="p-4">
         <Pagination itemsPerPage={10} totalItems={list?.length} />
