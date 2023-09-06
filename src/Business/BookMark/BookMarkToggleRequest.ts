a4197beeac0a874b4cde039f4031cf2c7f2ec2c6
fefdfea1 <46808357+fefdfea1@users.noreply.github.com>
2023-09-06 23:55:56 +0900

    Update BookMarkToggleRequest.ts

--- src/Business/BookMark/BookMarkToggleRequest.ts
+++ src/Business/BookMark/BookMarkToggleRequest.ts
@@ -1,5 +1,5 @@
-import { addBookMark } from "../../fetch/post/agent";
-import { deleteBookmark } from "../../fetch/post/agent";
+// import { addBookMark } from "../../fetch/post/agent";
+// import { deleteBookmark } from "../../fetch/post/agent";
 //만약 오피스 id 정보가 get 요청으로 함께 온다면 data- 로 저장하여 가져와서 전달
 export const toggleBookMarkRequest = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
   const target = event.target as HTMLElement;
