9b2e496c092a6022d2942c38beb461d9ec412d08
Jieun Kang <ochoby@gmail.com>
2023-09-13 20:37:50 +0900

    충돌 변경

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,16 +1,19 @@
-import styled from '@emotion/styled';
-import { useState } from 'react';
-import { modalCheckValue } from './ModalValueCheck';
+import styled from "@emotion/styled";
+import { useState } from "react";
+import { modalCheckValue } from "./ModalValueCheck";
+import { fetchAddPoint } from "../../fetch/post/customer";
+import { reFetchingType } from "../../pages/MyPage";
 
 type WrapperProps = {
   children: React.ReactNode;
   spanString: string;
   Pstring: string;
   buttonString: string;
+  refetch: reFetchingType;
 };
 
-export const Modal = ({ children, spanString, Pstring, buttonString }: WrapperProps) => {
-  const [inputValue, setInputValue] = useState<string>('');
+export const Modal = ({ children, spanString, Pstring, buttonString, refetch }: WrapperProps) => {
+  const [inputValue, setInputValue] = useState<string>("");
 
   return (
     <div className="w-96 h-60 p-8 shadow-xl box-content rounded-xl bg-secondary Modal  sm:w-64 sm:h-48  md:w-80 md:h-52  xl:w-96 xl:h-60">
@@ -27,16 +30,21 @@ export const Modal = ({ children, spanString, Pstring, buttonString }: WrapperPr
             }}
             value={inputValue}
           />
-          <SapnTag className="absolute">{spanString}</SapnTag>
+          <SpanTag className="absolute">{spanString}</SpanTag>
         </div>
       </div>
-      <button className="btn btn-primary w-full p-2.5 bg-primary rounded-lg text-base">{buttonString}</button>
+      <button
+        className="btn btn-primary w-full p-2.5 bg-primary rounded-lg text-base"
+        onClick={() => fetchAddPoint(inputValue, refetch)}
+      >
+        {buttonString}
+      </button>
     </div>
   );
 };
 // transform translate가 tailwind에서 -50% -50%로 정해진 틀이 없어서 직접
 // 스타일링 했습니다
-const SapnTag = styled.span`
+const SpanTag = styled.span`
   position: absolute;
   top: 50%;
   right: 10px;
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ export default ({ mode }) => {
   return {
     plugins: [
       react(),
-
       createHtmlPlugin({
         minify: true,
         inject: {
